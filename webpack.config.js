f873a1e37d5fa097b74e9a28343f9bdfd856cf54
Arpit Tomar <arpittomar7740@gmail.com>
2020-08-24 21:31:59 +0530

    wepack cli

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: "./src/js/index.js",
-    output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: "bundle.js"
-    },
-    mode: 'development'
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist/js"),
+    filename: "bundle.js",
+  },
+  mode: "development",
 };
