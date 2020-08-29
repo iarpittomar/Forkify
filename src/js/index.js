4eab50649a460f731e91fffa695ceb34a42cf16c
Arpit Tomar <arpittomar7740@gmail.com>
2020-08-30 02:05:13 +0530

    setup babel and axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
-console.log("safsafas");
+import axios from "axios";
+
+async function getResult(query) {
+  const res = await axios(
+    `https://forkify-api.herokuapp.com/api/search?q=${query}`
+  );
+  console.log(res);
+}
+
+getResult("pizza");
---
+++ src/js/models/Search.js
@@ -0,0 +1,9 @@
+export default "I am an exported string";
+
+// const res = await axios(
+//   `https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
+// );
+
+// const res = await axios(
+//   `https://forkify-api.herokuapp.com/api/get?rId=${this.id}`
+// );
