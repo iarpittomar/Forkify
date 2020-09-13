0f578483b54d2e3869a1677c9ba0eea277d46823
Arpit Tomar <arpittomar7740@gmail.com>
2020-09-13 16:10:30 +0530

    Finish list page

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -1,21 +1,20 @@
 import { elements } from "./base";
 
 export const renderItem = (item) => {
-  const markup = `
-                <li class="shopping__item" data-itemid=${item.id}>
-                    <div class="shopping__count">
-                        <input type="number" value="${item.count}" step="${item.count}">
-                        <p>${item.unit}</p>
-                    </div>
-                    <p class="shopping__description">${item.ingredient}</p>
-                    <button class="shopping__delete btn-tiny">
-                        <svg>
-                            <use href="img/icons.svg#icon-circle-with-cross"></use>
-                        </svg>
-                    </button>
-                </li>`;
+  const markup = `<li class="shopping__item" data-itemid=${item.id}>
+        <div class="shopping__count">
+            <input class="shopping__count-value" type="number" value="${item.count}" step="${item.count}" />
+            <p>${item.unit}</p>
+        </div>
+        <p class="shopping__description">${item.ingredient}</p>
+        <button class="shopping__delete btn-tiny">
+            <svg>
+                <use href="img/icons.svg#icon-circle-with-cross"></use>
+            </svg>
+        </button>
+    </li>`;
 
-  elements.shoppingList.insertAdjacentElement("beforeend", markup);
+  elements.shoppingList.insertAdjacentHTML("beforeend", markup);
 };
 
 export const deleteItem = (id) => {
